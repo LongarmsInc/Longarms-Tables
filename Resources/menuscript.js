91137bd65d75319879f804698795fd8d1e947aaf
MossW <59309429+MossW268@users.noreply.github.com>
2020-04-15 13:26:47 +0100

    Menu Bar
    I think it works now.

--- Resources/menuscript.js
+++ Resources/menuscript.js
@@ -1,15 +1,15 @@
 var check = "hey";
 var check = document.cookie;
 if (check == "hey") {
-  menu = "<a href="#" class="button">Sign Up</a>
-<a href="#" class="button">Log In</a>";
+  menu = '<a href="#" class="button">Sign Up</a>'+
+         '<a href="#" class="button">Log In</a>';
 } else {
-  menu = "<div class="icon-bar">
-            <a class="active" href="#"><i class="fas fa-home"></i></a>
-            <a href="#"><i class="fas fa-search"></i></a>
-            <a href="#"><i class="fas fa-table"></i></a>
-            <a href="#"><i class="far fa-envelope"></i></a>
-            <a href="#"><i class="fas fa-user-circle"></i></a>
-          </div>";
+  menu = '<div class="icon-bar">'+
+            '<a class="active" href="#"><i class="fas fa-home"></i></a>'+
+            '<a href="#"><i class="fas fa-search"></i></a>'+
+            '<a href="#"><i class="fas fa-table"></i></a>'+
+            '<a href="#"><i class="far fa-envelope"></i></a>'+
+            '<a href="#"><i class="fas fa-user-circle"></i></a>'+
+          '</div>';
 }
 document.getElementById("menuChoose").innerHTML = menu;
